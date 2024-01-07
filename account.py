dfe9aa9ea28ccc0333e7be8fd6c0136ccdcfbce9
Joeygjh <Joeykkhgjh@gmail.com>
2024-01-07 22:10:12 +0800

    feat: Login page UI and registration feature

--- account.py
+++ account.py
@@ -7,3 +7,24 @@ account_bp = Blueprint('account', __name__)
 @account_bp.route('/')
 def index():
     return render_template('index.html')
+
+@account_bp.route('/register', methods=['GET', 'POST'])
+def register():
+    if request.method == "POST":
+        # Get username and password inputs
+        username = request.form['username']
+        password = request.form['password']
+
+        # Connect to database
+        connection = get_database_connection()
+        cursor = connection.cursor()
+
+        # SQL Query to check login credentials and status
+        query = "INSERT INTO accounts (username, password, creation_date, account_type, approval_status) VALUES (%s, %s, %s, 'Normal User', 'Pending')"
+        # Get current datetime
+        current_date = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+        cursor.execute(query, (username, password, current_date))
+        connection.commit()
+        return render_template('register.html', message='Successful registration')
+    else:
+        return render_template('register.html')
