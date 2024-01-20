4275e8e60fa59151cfd5f56deae7847ac51c2a39
ThuYasWe <s10222373@connect.np.edu.sg>
2024-01-20 18:01:55 +0800

    feat: Added account management GET feature, account details Page UI, Approve Account POST feature.  update: Capstone Details

--- account.py
+++ account.py
@@ -1,13 +1,15 @@
-from flask import Blueprint, render_template, request, session
+from flask import Blueprint, render_template, request, session, redirect, url_for
 from database import get_database_connection
 from datetime import datetime
 
 account_bp = Blueprint('account', __name__)
 
+# Index 
 @account_bp.route('/')
 def index():
     return render_template('index.html')
 
+# Login GET & POST 
 @account_bp.route('/login', methods=['GET', 'POST'])
 def login():
     if request.method == "POST":
@@ -44,7 +46,8 @@ def login():
             return render_template('login.html', error='Invalid credentials')
     else:
         return render_template('login.html')
-    
+
+# Register GET & POST
 @account_bp.route('/register', methods=['GET', 'POST'])
 def register():
     if request.method == "POST":
@@ -66,3 +69,61 @@ def register():
     else:
         return render_template('register.html')
 
+# Dashboard GET
+@account_bp.route('/dashboard', methods=['GET']) 
+def dashboard():
+    # Get account type from session
+    account_type = session.get('account_type')
+    account_name = session.get('account_username')
+
+    if account_type == 'Normal User':
+        return render_template('dashboard.html', accountName=account_name, account_type='Normal User')
+    elif account_type == 'Administrator':
+        return render_template('dashboard.html', accountName=account_name, account_type='Administrator')
+    else:
+        return render_template('login.html')
+
+# Account Management GET
+@account_bp.route('/accountManagement', methods=['GET'])
+def accountManagement():
+    # Connect to database
+    connection = get_database_connection()
+    cursor = connection.cursor()
+
+    #SQL Query base
+    query = "SELECT * FROM accounts"
+
+    # Execute the SQL Query
+    cursor.execute(query)
+    accounts = cursor.fetchall()
+
+    return render_template('accountManagement.html', accounts=accounts)
+
+# Account Details GET
+@account_bp.route('/accountDetails/<int:acc_id>', methods=['GET'])
+def accountDetails(acc_id, message=''):
+    # Connect to the database
+    connection = get_database_connection()
+    cursor = connection.cursor()
+
+    # Query the specific account by ID
+    query = "SELECT * FROM accounts WHERE account_id = %s"
+    cursor.execute(query, (acc_id,))
+    account = cursor.fetchone()
+
+    return render_template('accountDetails.html', account=account, message=message)
+
+# Approve Account POST
+@account_bp.route('/approveAccount/<int:acc_id>', methods=['POST'])
+def approveAccount(acc_id):
+    # Connect to database
+    connection = get_database_connection()
+    cursor = connection.cursor()
+
+    #SQL Query base
+    query = """UPDATE accounts SET approval_status='Approved' WHERE account_id=%s"""
+
+    cursor.execute(query, (acc_id,))
+    connection.commit()
+
+    return accountManagement()
